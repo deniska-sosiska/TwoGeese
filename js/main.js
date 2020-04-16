ecf33559f54d0a04f402010e93cb363d8568664e
deniska-sosiska <denchikarabik@gmail.com>
2020-04-16 18:55:46 +0300

    День 4, Дело сделано

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-$('');
+$(function() {
+  $('.filter > ul > li').click(function(){
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      $(this.children[0]).removeClass('active');
+    } else {
+      $(this).addClass('active');
+      $(this.children[0]).addClass('active');
+    }
+  });
+  return  false;
+});
