18a04d325851834bfeae2a10c663437bd39d7500
deniska-sosiska <denchikarabik@gmail.com>
2020-04-22 18:46:22 +0300

    try to fix

--- js/main.js
+++ js/main.js
@@ -10,18 +10,18 @@ $(function() {
   });
   return  false;
 });
-$( function() {
-  $( "#slider-range" ).slider({
-    animate: "fast",
-    range: true,
-    min: 700,
-    max: 15000,
-    values: [ 700, 15000 ],
-    slide: function( event, ui ) {
-      $( "#amount-1" ).val(ui.values[ 0 ]);
-      $( "#amount-2" ).val(ui.values[ 1 ]);
-    }
-  });
-  $( "#amount-1" ).val($( "#slider-range" ).slider( "values", 0 ));
-  $( "#amount-2" ).val($( "#slider-range" ).slider( "values", 1 ));
-});
+// $( function() {
+//   $( "#slider-range" ).slider({
+//     animate: "fast",
+//     range: true,
+//     min: 700,
+//     max: 15000,
+//     values: [ 700, 15000 ],
+//     slide: function( event, ui ) {
+//       $( "#amount-1" ).val(ui.values[ 0 ]);
+//       $( "#amount-2" ).val(ui.values[ 1 ]);
+//     }
+//   });
+//   $( "#amount-1" ).val($( "#slider-range" ).slider( "values", 0 ));
+//   $( "#amount-2" ).val($( "#slider-range" ).slider( "values", 1 ));
+// });
